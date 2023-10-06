310be06641bd0a0c3d249f68ca10f3cb1e74fd03
maywang1214 <112986613+maywang1214@users.noreply.github.com>
2023-10-06 11:50:04 -0700

    2.2.3 Structuring the Project Completed

---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    private float spawnRange;
+    private float startDelay = 2.0f;
+    private float spawnInterval = 1.5f;
+    [SerializeField] private GameObject[] animals;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //to be implemented
+    }
+
+    private void SpawnRandomAnimal()
+    {
+        //to be implemented
+    }
+}
